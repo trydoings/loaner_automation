d49f1183a88023b1bdec939be7b8ef2446b1e4af
Austin Harshberger <aharshbe@github.com>
2019-02-27 14:41:04 -0800

    fixing test username + password change script

--- files/test.sh
+++ files/test.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
 # Obtain variables from UI and assign them to more understandable values
-# fullname=$1
-# handle=$2
-# password=$3
-#
-#
-# echo "Full name: $fullname Handle: $handle Password: $password"
+fullname=$1
+handle=$2
+password=$3
+
+echo "Full name: $fullname Handle: $handle Password: $password"
 
 ShortName=`/usr/bin/who | awk '/console/{ print $1 }' | tail -n 1`
 FullName=`dscl . -read /Users/$ShortName RealName | tail -1`
 
-echo $ShortName
-echo $FullName
+sudo dscl . change /Users/$ShortName RealName $FullName $handle
+sudo dscl . -passwd /Users/$handle $password
 
-sudo dscl . change /Users/aharshbe RealName "Austin Harshberger" "aharshbe"
+echo "User credentials changed."
+
+# Log out
+osascript -e 'tell application "loginwindow" to  «event aevtrlgo»'
