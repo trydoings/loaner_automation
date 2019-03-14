2236bf3f50553d47f532902c99633f798f32664e
Austin Harshberger <aharshbe@github.com>
2019-03-14 11:24:14 -0700

    adding changes to test.sh file

--- files/test.sh
+++ files/test.sh
@@ -16,12 +16,14 @@ if [[ $model == *"MacBook"* ]]; then
 fi
 
 data="$fullname,$handle,$password,$oldpassword,$serialNumber,$model,$type"
+json_data='{"serial":"'$serialNumber'","model":"'$model'","type":"'$type'","owners":{"handle":"'$handle'","user_name":"'$fullname'","password":"'$password'","checkin":"default","checkout":"default"},"date_modified":{"created_at":"default","modified":"default"},"age":"default","qr":"default","reset":"default","expired":"default"}'
 
 printf "\nCredentials to be sent:\n
 Full name: $fullname\nHandle: $handle\nPassword: $password\nOldpassowrd: $oldpassword\nserialNumber: $serialNumber\nmodel: $model\ntype: $type\n\n"
 
 # Creates a put request to send user data to Node server
 curl -X PUT localhost:3000/test/make?new_user=$data
+# curl -H 'Content-Type: application/json' -X PUT -d "$json_data" -g localhost:3000/test/make
 # Uses the user's credntials to change the current account login and password
 # ShortName=`whoami`
 # FullName=`dscl . -read /Users/$ShortName RealName | tail -1 | sed 's/[^ ]* //'`
