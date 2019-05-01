ae40972155838698ecc974bf5bf1eab3aba47895
A H <aharshbe@github.com>
2019-05-01 16:41:35 -0700

    Update checkin_qr.sh

--- files/checkin_qr.sh
+++ files/checkin_qr.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 serialNumber=$(ioreg -l | awk '/IOPlatformSerialNumber/ { print $4;}' | sed -e 's/^"//' -e 's/"$//')
+server=$(cd .. && pwd)
+# Go back to previous directory
+cd - >/dev/null
 val=$(osascript -e '
-set icon to POSIX file "/Users/'$(whoami)'/Desktop/LA/loaner_automation/server/qr/'$serialNumber'.icns"
+set icon to POSIX file "'$server'/server/qr/'$serialNumber'.png"
 set icon to icon as alias
 display dialog "Scan QR code to check in loaner machine." buttons {"Non-QR check in", "Done"} default button "Done" with icon icon')
 if [[ $val == "button returned:Done" ]];
